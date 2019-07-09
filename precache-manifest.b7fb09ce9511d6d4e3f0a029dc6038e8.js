0bbbe77da84d8a9e29fbedaddd46bfcbd5064d9d
Dosty <dev.dosty@gmail.com>
2019-07-09 06:34:11 -0500

    Updates

---
+++ precache-manifest.b7fb09ce9511d6d4e3f0a029dc6038e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de55f31c80e9c788f682d9696945df9",
+    "url": "/wow-party/index.html"
+  },
+  {
+    "revision": "12d72052e61b1ead707b",
+    "url": "/wow-party/static/css/main.755b01fd.chunk.css"
+  },
+  {
+    "revision": "dfb213451ece820f700d",
+    "url": "/wow-party/static/js/2.752f5bd8.chunk.js"
+  },
+  {
+    "revision": "12d72052e61b1ead707b",
+    "url": "/wow-party/static/js/main.7cb7d8ba.chunk.js"
+  },
+  {
+    "revision": "c74ad2764554d836fd30",
+    "url": "/wow-party/static/js/runtime~main.cb5c69e3.js"
+  }
+]);
