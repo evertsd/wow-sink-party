73acf2567bca36a6d6ba91f0bec0d8df98bc693f
Dosty <dev.dosty@gmail.com>
2019-08-12 20:52:59 -0500

    Updated sync party lambda to only fetch data from bnet if the character hasn't been updated in an hour

--- soap/src/firebase/index.ts
+++ soap/src/firebase/index.ts
@@ -1,5 +1,9 @@
+import * as firebase from 'firebase-admin';
 import * as Character from './character';
 import * as Connection from './connection';
 import * as Party from './party';
 
 export { Character, Connection, Party };
+
+export const mapTimestampToDate = (timestamp: firebase.firestore.Timestamp) =>
+  new firebase.firestore.Timestamp(timestamp.seconds, timestamp.nanoseconds).toDate();
--- soap/src/services/party/sync.ts
+++ soap/src/services/party/sync.ts
@@ -1,11 +1,11 @@
+import moment from 'moment';
 import * as bnet from '~/battle-net';
 import * as firebase from '~/firebase';
 import { notUndefined } from '../filter';
 
 export const perform = async (id: string, token: bnet.api.AccessToken) => {
-  console.info('perform', id);
   const party = await firebase.Party.get(id);
-  console.info('party', party);
+
   const characterUpdates = (await Promise.all(
     party.members.map(member => getCharacter(member, token)),
   )).filter(notUndefined);
@@ -15,7 +15,7 @@ export const perform = async (id: string, token: bnet.api.AccessToken) => {
 
   if (didUpdate) {
     firebase.Party.update(id, { modifiedAt: firebase.Connection.now() });
-    console.info('party.modifiedAt', party.modifiedAt, firebase.Connection.now());
+
     party.modifiedAt = firebase.Connection.now();
   }
 
@@ -31,6 +31,7 @@ export const getCharacter = async (
 
   try {
     const character = await firebase.Character.getOrDefault(id, undefined);
+    if (character && !shouldUpdateCharacter(character)) { return [character, false]; }
     const data = await bnet.Character.get(realm, name, { region, access_token });
 
     const updatedCharacter: firebase.Character.Model = {
@@ -39,19 +40,11 @@ export const getCharacter = async (
       createdAt: (character && character.createdAt) || firebase.Connection.now(),
       modifiedAt: firebase.Connection.now(),
     };
-    console.info(
-      'character.lastLoginTimestamp, updatedCharacter.lastLoginTimestamp',
-      id,
-      character ? character.lastLoginTimestamp : 'No last login',
-      updatedCharacter.lastLoginTimestamp,
-    );
 
     const willUpdate = !character || updatedCharacter.lastLoginTimestamp !== character.lastLoginTimestamp || true;
 
     if (willUpdate) {
-      console.info('firebase.Character.set', updatedCharacter);
       await firebase.Character.set(id, updatedCharacter);
-      console.info('firebase.Character.updated!');
     }
 
     return [updatedCharacter, willUpdate];
@@ -59,6 +52,13 @@ export const getCharacter = async (
     console.error(e);
     return undefined;
   }
+};
+
+const shouldUpdateCharacter = (character: firebase.Character.Model) => {
+  if (!(character.modifiedAt)) { return true; }
+  const msSinceUpdate = moment().diff(firebase.mapTimestampToDate(character.modifiedAt));
+
+  return moment.duration(msSinceUpdate).asHours() > 1;
 };
 
 export const mapToFirebaseModel = (character: bnet.Character.Model): firebase.Character.Model => ({
